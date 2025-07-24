a2b2839ac89ea40c3a54bb4e8daf67a264060190
w30021754 <wangguohua6@huawei.com>
2025-07-24 08:33:35 +0800

    update createContext

--- packages/trpc/src/context.ts
+++ packages/trpc/src/context.ts
@@ -2,4 +2,11 @@
  * 提供请求时的上下文，比如用户信息、请求头等。
  */
 
-export type Context = {};
+export function createContext() {
+  // mock
+  return {
+    userId: 'anonymous-user',
+  };
+}
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
