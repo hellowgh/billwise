c1f22137be9053c2113c5952607648c797fda7c1
w30021754 <wangguohua6@huawei.com>
2025-07-24 09:01:26 +0800

    inject context

--- apps/web/app/api/trpc/[trpc]/route.ts
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -1,5 +1,5 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { appRouter } from '@billwise/trpc';
+import { appRouter, createContext } from '@billwise/trpc';
 import { NextRequest } from 'next/server';
 
 const handler = (req: NextRequest) => {
@@ -7,7 +7,7 @@ const handler = (req: NextRequest) => {
     endpoint: '/api/trpc',
     req,
     router: appRouter,
-    createContext: () => ({}), // 简化：无 auth
+    createContext,
   });
 };
 
--- packages/trpc/src/routers/hello.ts
+++ packages/trpc/src/routers/hello.ts
@@ -1,7 +1,7 @@
 import { publicProcedure, router } from '../trpc';
 
 export const testRouter = router({
-  hello: publicProcedure.query(() => {
-    return { message: 'hello trpc' };
+  hello: publicProcedure.query(({ ctx }) => {
+    return `Hello ${ctx.userId}`;
   }),
 });
